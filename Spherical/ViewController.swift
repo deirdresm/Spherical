170f39b7a70fd492ce14e361f3cee0f13a03910c
Deirdre Saoirse Moen <deirdre@deirdre.net>
2018-10-25 10:31:45 -0700

    Initial Commit

---
+++ Spherical/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spherical
+//
+//  Created by Deirdre Saoirse Moen on 10/25/18.
+//  Copyright © 2018 Deirdre Saoirse Moen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
