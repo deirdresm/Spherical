da6ce0a0998a82a245c319fd8a333277cc958bbe
Deirdre Saoirse Moen <deirdre@deirdre.net>
2019-03-11 00:39:08 -0700

    Starting a refactor to scoop some bits into a struct.

---
+++ Spherical/Models/MakerMeta.swift
@@ -0,0 +1,17 @@
+//
+//  MakerMeta.swift
+//  Spherical
+//
+//  Created by Deirdre Saoirse Moen on 3/11/19.
+//  Copyright © 2019 Deirdre Saoirse Moen. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+struct MakerMeta {
+	var maker : Maker
+	var nameNode : SCNNode
+	var colorNodes : [SCNNode]
+	var labelColor : UIColor
+}
